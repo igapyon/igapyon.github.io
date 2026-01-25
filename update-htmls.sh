1cf075316ef6a85a547c2bba853ca7cc6fe81bff
Toshiki Iga <igapyon@gmail.com>
2026-01-25 21:18:43 +0900

    アプリ集を追加 (#3)
    アプリ集を追加

---
+++ update-htmls.sh
@@ -0,0 +1,3 @@
+cd /var/www/html
+wget -O update-html.sh https://raw.githubusercontent.com/igapyon/igapyon.github.io/devel/update-html.sh
+wget -O index.html https://raw.githubusercontent.com/igapyon/igapyon.github.io/devel/index.html
