4e18239b3f4d79b7b8d49daf096230a80ab10dc9
Toshiki Iga <igapyon@gmail.com>
2026-01-25 21:21:59 +0900

    ファイルコピーの機構を調整中 (#4)
    ファイルコピーの機構を調整中

--- update-htmls.sh
+++ update-htmls.sh
@@ -1,3 +1,3 @@
 cd /var/www/html
-wget -O update-html.sh https://raw.githubusercontent.com/igapyon/igapyon.github.io/devel/update-html.sh
+wget -O update-htmls.sh https://raw.githubusercontent.com/igapyon/igapyon.github.io/devel/update-htmls.sh
 wget -O index.html https://raw.githubusercontent.com/igapyon/igapyon.github.io/devel/index.html
