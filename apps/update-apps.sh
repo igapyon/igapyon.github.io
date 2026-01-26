c8290da0394cd9e0676af27e0379218357ac06f9
igapyon <igapyon@mac.lan>
2026-01-26 20:53:36 +0900

    save

--- apps/update-apps.sh
+++ apps/update-apps.sh
@@ -1,1 +1,1 @@
-wget -O klondike.html https://raw.githubusercontent.com/igapyon/klondike/devel/index-offline.html
+wget -O klondike.html https://raw.githubusercontent.com/igapyon/klondike/devel/index.html
